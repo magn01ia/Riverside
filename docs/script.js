2a8e07da85260d25c30feb5feef2f750a412c861
magn01ia <84883260+magn01ia@users.noreply.github.com>
2023-02-06 08:53:23 +0900

    Create script.js

---
+++ docs/script.js
@@ -0,0 +1,26 @@
+ var map = new maplibregl.Map({
+    container: "map",
+    style: {
+      version: 8,
+      sources: {
+        rtile: {
+          type: "raster",
+          tiles: ["https://tile.openstreetmap.jp/{z}/{x}/{y}.png"],
+          tileSize: 256,
+          attribution:
+            '<a href="https://www.openstreetmap.org/copyright" target="_blank">&copy; OpenStreetMap contributors</a>'
+        }
+      },
+      layers: [
+        {
+          id: "raster-tiles",
+          type: "raster",
+          source: "rtile",
+          minzoom: 0,
+          maxzoom: 22
+        }
+      ]
+    },
+    center: [140.8830781, 38.2615922],
+    zoom: 13
+  });
